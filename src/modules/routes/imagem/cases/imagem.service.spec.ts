419b9f0334d2ad422eebc120dce3636e71faa297
raioramalho <ramalho.sit@gmail.com>
2023-11-28 14:56:00 -0300

    comentei o imagem service

--- src/modules/routes/imagem/cases/imagem.service.spec.ts
+++ src/modules/routes/imagem/cases/imagem.service.spec.ts
@@ -1,44 +1,44 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import ImagemService from './imagem.service';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import ImagemService from './imagem.service';
 
-describe('ImagemService', () => {
-  let service: ImagemService;
+// describe('ImagemService', () => {
+//   let service: ImagemService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ImagemService],
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [ImagemService],
+//     }).compile();
 
-    service = module.get<ImagemService>(ImagemService);
-  });
+//     service = module.get<ImagemService>(ImagemService);
+//   });
 
-  it('Deve ser denifido', () => {
-    expect(service).toBeDefined();
-  });
+//   it('Deve ser denifido', () => {
+//     expect(service).toBeDefined();
+//   });
 
-  it('Deve baixar uma imagem e salvar na pasta public', async () => {
-    const imagem = {
-      src: 'https://i.pinimg.com/564x/e0/c6/15/e0c6153b97b60a9ccb082b9768b27816.jpg',
-      name: 'imagem-1.jpg',
-    };
+//   it('Deve baixar uma imagem e salvar na pasta public', async () => {
+//     const imagem = {
+//       src: 'https://i.pinimg.com/564x/e0/c6/15/e0c6153b97b60a9ccb082b9768b27816.jpg',
+//       name: 'imagem-1.jpg',
+//     };
 
-    const download: any = await service.download(
-      imagem.src,
-      'public',
-      imagem.name,
-    );
+//     const download: any = await service.download(
+//       imagem.src,
+//       'public',
+//       imagem.name,
+//     );
 
-    expect(download.status).toBe(true);
-  });
+//     expect(download.status).toBe(true);
+//   });
 
-  it('Deve buscar uma imagem existente na pasta public', async () => {
-    const imagem = {
-      src: 'https://i.pinimg.com/564x/e0/c6/15/e0c6153b97b60a9ccb082b9768b27816.jpg',
-      name: 'imagem-1.jpg',
-    };
+//   it('Deve buscar uma imagem existente na pasta public', async () => {
+//     const imagem = {
+//       src: 'https://i.pinimg.com/564x/e0/c6/15/e0c6153b97b60a9ccb082b9768b27816.jpg',
+//       name: 'imagem-1.jpg',
+//     };
 
-    const find = await service.findImage(`public/${imagem.name}`);
+//     const find = await service.findImage(`public/${imagem.name}`);
 
-    expect(find).toBe(true);
-  });
-});
+//     expect(find).toBe(true);
+//   });
+// });
--- src/modules/routes/imagem/imagem.module.ts
+++ src/modules/routes/imagem/imagem.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { ImagemController } from './imagem.controller';
 import { DownloadImagemService } from './cases/download.imagem.service';
-import ImagemService from './cases/imagem.service';
 
 @Module({
   controllers: [ImagemController],
-  providers: [ImagemService, DownloadImagemService],
+  providers: [DownloadImagemService],
 })
 export class ImagemModule {}
