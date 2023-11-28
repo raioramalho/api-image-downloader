c42438610c0310db5b9abd2fc550130ede97c4ca
raioramalho <ramalho.sit@gmail.com>
2023-11-28 15:49:31 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(
